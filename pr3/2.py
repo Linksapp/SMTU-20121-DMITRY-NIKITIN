63614d0f52e3bcd59db049fd15776b2c81d57f85
Linksapp <nikitindima1500@gmail.com>
2023-01-06 22:20:56 +0300

    now it's working

--- pr3/2.py
+++ pr3/2.py
@@ -1,18 +1,26 @@
-# doesn't work
 if __name__ == '__main__':
-    N = input()
+    N = input().split()
     n = len(N)
     new_N = []
-    fact_N = 1
-    while n > 1:
-        fact_N *= n
-        n -=1 
-    N_list = []
-    position = 0
-    for x in range(fact_N):
-        for j in range(fact_N // number):
-            new_N.append([])
-            new_N.append[position](N[x%4])
-            position += 1
-        number = len(N)
-        
+    temp = []
+    buffer = []
+    if n == 1:
+        new_N.append(int(N[0]))
+    else:
+        if new_N == []:
+            new_N.append([[0 for i in range(n)]])
+        for y in N:
+            for n_list in new_N:
+                for un_n_list in n_list:
+                    for x in range(len(un_n_list)):  
+                        if un_n_list[x] == 0:
+                            temp = un_n_list.copy()            
+                            temp[x] = int(y)
+                            if temp not in buffer:
+                                buffer.append(temp)
+            new_N.append(buffer)
+            buffer = []
+            del new_N[0]
+        new_N = new_N[0]
+    print(new_N)
+    
