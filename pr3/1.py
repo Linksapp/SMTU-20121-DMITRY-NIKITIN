e8e77fb388a64f5f2d130680f5945fe552b56336
Linksapp <nikitindima1500@gmail.com>
2023-01-07 13:51:15 +0300

    now it's working

--- pr3/1.py
+++ pr3/1.py
@@ -1,15 +1,19 @@
-# doesn't work 
+from itertools import permutations as permut
+
 if __name__ == '__main__':
     N = int(input())
-    input_numbers = [int(input()) for x in range(N)]
+    input_numbers = [int(input()) for _ in range(N)]
     C = int(input())
     goal_list = []
-    while input_numbers:
-        if C > 0:
-            if min(input_numbers) > C and min(input_numbers) > 0:
-                goal_list.append(min(input_numbers))
-                input_numbers.remove(min(input_numbers))
-            # goal_list.append(max(input_numbers))
-            # input_numbers.remove(max(input_numbers))
-            # if sum(goal_list) > 
-            else: input_numbers.remove(min(input_numbers))
+    goal_sum = 10**10
+    members = list(permut(input_numbers, 4))
+    for uniq in members:
+        if C == 0:
+            if abs(sum(uniq)) < goal_sum:
+                goal_list = uniq
+                goal_sum = sum(uniq)
+        if abs(C - sum(uniq)) < goal_sum:
+            goal_sum = abs(C - sum(uniq))
+            goal_list = uniq
+    print(list(goal_list))
+    print(sum(goal_list))
