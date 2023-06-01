ba5cb05ba11294b08dfb77483127874dd162f24e
Linksapp <nikitindima1500@gmail.com>
2023-06-01 13:15:03 +0300

    add User class

--- semester 2/pr1/2.py
+++ semester 2/pr1/2.py
@@ -1,9 +1,14 @@
-users_catalog = {}
-test_users = [('Joe', 'myemail@gmail.com', 19), ('Bob', 'm@gmail', 25), ('John', 'john2010@gmail.com', 13), ('Joe', 'jo_e@gmail.com', 23)]
+class User:
+    def __init__(self, name, email):
+        self.name = name 
+        self.email = email
+
 
 class Catalog:
     def __init__(self, data: list[tuple[str, str, int]]):
         self.data = data
+        self.user_catalog = {}
+
     @staticmethod
     def _email_check(mail):
         if '@' in mail and len(mail[:mail.find('@')]) > 1 and len(mail[mail.find('@'):]) > 1 and '.' in mail[mail.find('@'):]:
@@ -13,14 +18,16 @@ class Catalog:
     def checker(self):
         for user in self.data:
             try:
-                assert not(user[0] in users_catalog), "This username is already taken"
+                assert not(user[0] in self.user_catalog), "This username is already taken"
                 assert user[2] >= 16, "You have to be over 16"
                 assert Catalog._email_check(user[1]), "Such email adress doesn't exist"
-                users_catalog[f'{user[0]}'] = (user[1], user[2])
+                self.user_catalog[f'{user[0]}'] = User(user[0], user[1])
             except AssertionError as error:
                 print(error)
                 continue
+
+test_users = [('Joe', 'myemail@gmail.com', 19), ('Bob', 'm@gmail', 25), ('John', 'john2010@gmail.com', 13), ('Joe', 'jo_e@gmail.com', 23)]
     
 example = Catalog(test_users)
 example.checker()
-print(users_catalog)
+print(example.user_catalog)
