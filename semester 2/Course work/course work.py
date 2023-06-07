cc4abe01aafc71dd7612d45fdafebbb8d4b07ff3
Linksapp <nikitindima1500@gmail.com>
2023-06-07 13:28:22 +0300

    add typing

--- semester 2/Course work/course work.py
+++ semester 2/Course work/course work.py
@@ -2,6 +2,7 @@ import sys
 import customtkinter as ctk
 import pygame
 from time import time
+from typing import Callable
 pygame.init()
 
 BLACK = (0, 0, 0)
@@ -14,14 +15,16 @@ FPS = 60
 
 
 class Board:
-    def __init__(self, board_size: int):
+    '''Класс, отвечающий за создание доски'''
+    def __init__(self, board_size: int) -> None:    
         self.board_size = board_size
         self.width = int(500 / self.board_size)
         self.height = int(500 / self.board_size)
         self.margin = 5
         self.board_grid = [self.board_size * [0] for x in range(self.board_size)]
 
-    def fill_board_grid(self, surface, grid, size):
+    def fill_board_grid(self, surface: pygame.Surface, grid: list[str], size: int) -> None:
+        '''Метод, рисующий доску '''
         for row in range(size):
                 for column in range(size):
                     self.color = WHITE
@@ -35,8 +38,10 @@ class Board:
                               (self.margin + self.height) * row + self.margin,
                               self.width,
                               self.height])
+                    
 
-    def attacked_cell(self, x: int, y: int, board_size: int) -> list:
+    def attacked_cell(self, x: int, y: int, board_size: int) -> list[tuple[int, int]]:
+        '''Метод находит атакованные клетки и возвращает список кортежей с коррдинатами'''
         self.attacked = []
         for num in range(1, 3):
             if (x - num) >= 0 and (y - num) >= 0:
@@ -49,13 +54,16 @@ class Board:
                 self.attacked.append((x + num, y + num))
         return self.attacked
     
-class Figure:
-    ...
-
 class Input_window(ctk.CTk):
-    def __init__(self, x=700, y=500) -> None:
+    '''
+    Класс, отвечающий за ввод необходимых данных
+    содержит 2 поля ввода:
+    Размер доски
+    Количество фигур
+    '''
+    def __init__(self) -> None:
         super().__init__()
-        self.num_size_check = (self.register(self._do_size_validation), '%P')
+        self.num_size_check = (self.register(self._do_size_validation), '%P') # %P новое значение, которое будет, если текст пройдёт валидацию; register создает новую Tcl команду, которая вызывает команду python
         self.num_figures_check = (self.register(self._do_figures_validation), '%P')
         self.board_size_label = ctk.CTkLabel(self, text='Размер доски: ')
         self.board_size_entry = ctk.CTkEntry(self, validate='key', validatecommand=self.num_size_check, border_width=0)
@@ -69,37 +77,51 @@ class Input_window(ctk.CTk):
         self.get_button.grid(row=2, column=3)
 
     def _create_pygame_window(self) -> None:
+        '''Создаёт pygame окно, содержащее доску и кнопку'''
         if self.board_size_entry.get() != '' and self.number_of_figures_entry.get() != '':
-            board_window = Pygame_board_window(int(self.board_size_entry.get()), int(self.number_of_figures_entry.get()))
+            self.board_window = Pygame_board_window(int(self.board_size_entry.get()), int(self.number_of_figures_entry.get()))
             self.destroy()
-            board_window.make_board()
+            self.board_window.make_board()
         
 
     def _do_size_validation(self, new_value: str) -> bool:
+        '''Проверяет вводимые в self.board_size_entry данные'''
         if new_value.isnumeric():
             return len(new_value) <= 2 and (int(new_value) <= 20) and new_value[0] != '0'
         else: return new_value == ''
     def _do_figures_validation(self, new_value: str) -> bool:
+        '''Делает такие же действия, как _do_size_validation, 
+        но с изменёнными условиями и проверяет вводимы в self.number_of_figures_entry'''
         if new_value.isnumeric():
             return len(new_value) <= 2 and new_value != '00'
         else: return new_value == '' 
 
 
 class Pygame_button:
-    def __init__(self, width, height):
+    '''Класс отвечающий за создание кнопки в pygame'''
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
 
-    def make_button(self, text, text_size=26):
+    def make_button(self, text: str, text_size: int=26) -> pygame.Surface:
+        '''Создаёт pygame.Surface, выполняющую роль кнопки'''
         self.font = pygame.font.SysFont('timesnewroman', text_size)
         button = self.font.render(text, True, BLACK, SILVER)
         return button
     
-    def get_clicked(self, action=None, *args):
+    def get_clicked(self, action: Callable=None, *args: list[int, int, list[tuple[int, int]]]) -> None:
+        '''Совершает действие, если была нажата кнопка, созданная make_button'''
         action(*args)
 
 class Pygame_window:
-    def __init__(self, board_size: int, number_of_figures: int):
+    '''Родительский класс всех pygame окон, содержит необходимые команды и переменные'''
+    def __init__(self, board_size: int, number_of_figures: int) -> None:
+        '''
+        self.board_surface - создаёт pygame.Surface на которой удет располагаться доска
+        self.button_surface - создаёт pygame.Surface на которой будет располагаться кнопка
+        self.continue_button - является объектом класса Pygame_button
+        self.board_maker - является объектом класса Board 
+        '''
         self.board_size = board_size
         self.number_of_figures = number_of_figures
         self.width = int(500 / self.board_size)
@@ -116,7 +138,8 @@ class Pygame_window:
         self.board_maker = Board(self.board_size)
         self.board_grid = self.board_maker.board_grid
 
-    def blit_objects(self, text: str, grid):
+    def blit_objects(self, text: str, grid: list) -> None:
+        '''Отвечает отвечает за наполнение окна'''
         self.scr.blit(self.board_surface, (0, 0))
         self.scr.blit(self.button_surface, (0, self.side))
         self.board_maker.fill_board_grid(self.board_surface, grid, self.board_size)
@@ -124,6 +147,7 @@ class Pygame_window:
     
     @staticmethod
     def no_solution_window():
+        '''Статичный метод, который создаёт окно с надписью No solution и записывает эту же фразу в файл, если решение не было обнаружено'''
         scr = pygame.display.set_mode([500, 500])
         scr.fill(WHITE)
         font = pygame.font.SysFont('timesnewroman', 80)
@@ -136,10 +160,78 @@ class Pygame_window:
                     quit()  
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     quit()
-            pygame.display.update()       
+            pygame.display.update() 
+
+class Pygame_board_window(Pygame_window):
+    '''Класс, отвечающий за окно с доской, на которой можно расставить фигуры'''
+    def __init__(self, board_size: int, number_of_figures: int):
+        super().__init__(board_size, number_of_figures)
+        self.coords = []
+        self.all_attacked = {}
+
+    def _create_output_window(self, size, number_of_figures, coords) -> None:
+        '''Создаёт pygame окно с первым найденным решение'''
+        self.run = False
+        self.output_window = Pygame_output_window(size, number_of_figures, coords)
+        self.output_window.display_first_solution()
+        
+
+    def make_board(self) -> None:
+        '''Отвечает за создание окна, на котором находится интерактивная доска и кнопка'''
+        pygame.display.set_caption('Board')
+        self.icon = pygame.image.load('semester 2\Course work\chess_icon.png')
+        pygame.display.set_icon(self.icon)
+        
+        while self.run:
+            self.blit_objects('Continue', self.board_grid)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+                    
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.pos = pygame.mouse.get_pos()
+                    self.column = self.pos[0] // (self.width + self.margin)
+                    self.row = self.pos[1] // (self.height + self.margin)
+                    if self.pos[0] in range(self.side-100, self.side-5) and self.pos[1] in range(self.side+5, self.side+40): 
+                        self.continue_button.get_clicked(self._create_output_window, self.board_size, self.number_of_figures, self.coords)
+                    if (self.row >= self.board_size or self.column >= self.board_size): continue
+                    self.attacked = self.board_maker.attacked_cell(self.row, self.column, self.board_size)
+
+                    if event.dict['button'] == 1:
+                        
+                        if not((self.row >= self.board_size or self.column >= self.board_size)):
+                            if self.board_grid[self.row][self.column] == '#' or self.board_grid[self.row][self.column] == '*':
+                                continue
+                            
+                            for cell in self.attacked:
+                                self.board_grid[cell[0]][cell[1]] = '*'
+                                if self.all_attacked.get(cell) == None:
+                                    self.all_attacked[cell] = 1
+                                else: self.all_attacked[cell] +=1
+
+                            self.coords.append((self.row, self.column))
+                            self.board_grid[self.row][self.column] = '#'
+
+
+                    elif event.dict['button'] == 3:
+                        if self.board_grid[self.row][self.column] == '#':
+                            for cell in self.attacked:
+                                self.all_attacked[cell] -= 1
+                                if self.all_attacked[cell] == 0: 
+                                    self.board_grid[cell[0]][cell[1]] = 0
+                                    del self.all_attacked[cell]
+
+                        if self.board_grid[self.row][self.column] == '#':
+                            del self.coords[self.coords.index((self.row, self.column))]
+                            self.board_grid[self.row][self.column] = 0
+
+
+            self.clock.tick(FPS)
+            pygame.display.update()      
 
 class Pygame_output_window(Pygame_window):
-    def __init__(self, board_size, number_of_figures, coords):
+    '''Класс отвечающий за окно, с первым найденным выводом'''
+    def __init__(self, board_size: int, number_of_figures: int, coords: list[tuple[int, int]]) -> None:
         super().__init__(board_size, number_of_figures)
         self.coords = coords
         self.xstart = 0
@@ -148,13 +240,13 @@ class Pygame_output_window(Pygame_window):
         self.solution(self.xstart, self.ystart, 
                                         self.putting_given_figure([self.board_size * [0] for x in range(self.board_size)], self.board_size, self.coords),
                                          self.number_of_figures, self.coords, True)
-        print(self.firstboard)
         if self.firstboard is not None:
             self.putting_given_figure([self.board_size * [0] for x in range(self.board_size)], self.board_size, self.firstboard)
         else:
             self.no_solution_window()
 
-    def display_first_solution(self):
+    def display_first_solution(self) -> None:
+        '''Метод отвечающий за вывод первого найденного решения и создание кнопки записи в файл всех решений'''
         while self.run:
             self.blit_objects('Записать', self.putting_given_figure([self.board_size * [0] for x in range(self.board_size)], self.board_size, self.firstboard))
             for event in pygame.event.get():
@@ -169,14 +261,11 @@ class Pygame_output_window(Pygame_window):
                                       self.number_of_figures, self.coords, False)
                         
                         self.btime = time() - self.atime
-                        output.seek(0) # воозвращаемся к началу файла, это необходимо, что бы len(output.readlines()) вывелось количество элементов
+                        output.seek(0) 
                         print(f'Количество решений: {len(output.readlines())}')
                         for _ in self.putting_given_figure(self.board_grid, self.board_size ,self.firstboard): print(*_)
                         print(f'Время работы {self.btime}')
                         sys.exit()
-
-                            
-
 
             self.clock.tick(FPS)
             pygame.display.update()
@@ -193,16 +282,11 @@ class Pygame_output_window(Pygame_window):
         else: 
             for x in range(xstart, len(board)): # xstart нужен, чтобы мы не проходились по уже пройденным клеткам
                 for y in range(len(board)):
-                    # print(x)
                     if x == xstart and y < ystart:  # эта проверка служит для того же
                         continue
                     if board[x][y] == 0 and self.under_attack(x, y, board):  # проверка является ли клетка 0 и находится ли она под ударом
                         board[x][y] = '#' # ставим фигуру
-                        # if first and fig_num == 0:
-                        #     return standing + [tuple([x, y])]
                         self.solution(x, y, board, fig_num - 1, standing + [tuple([x, y])], first)
-                        '''передаём координаты, оставшееся количество фигур, 
-                        которые надо поставить и уже стоящие фигуры'''
                         board[x][y] = 0  # убираем фигуру
 
     def under_attack(self, x: int, y: int, board: list) -> bool: 
@@ -227,70 +311,7 @@ class Pygame_output_window(Pygame_window):
         return board
 
 
-class Pygame_board_window(Pygame_window):
-    def __init__(self, board_size: int, number_of_figures: int):
-        super().__init__(board_size, number_of_figures)
-        self.coords = []
-        self.all_attacked = {}
 
-    def _create_output_window(self, size, number_of_figures, coords):
-        self.run = False
-        self.output_window = Pygame_output_window(size, number_of_figures, coords)
-        self.output_window.display_first_solution()
-        
-
-    def make_board(self) -> None:
-        pygame.display.set_caption('Board')
-        self.icon = pygame.image.load('semester 2\Course work\chess_icon.png')
-        pygame.display.set_icon(self.icon)
-        
-        while self.run:
-            self.blit_objects('Continue', self.board_grid)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    print(self.coords)
-                    quit()
-                    
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    self.pos = pygame.mouse.get_pos()
-                    self.column = self.pos[0] // (self.width + self.margin)
-                    self.row = self.pos[1] // (self.height + self.margin)
-                    if self.pos[0] in range(self.side-100, self.side-5) and self.pos[1] in range(self.side+5, self.side+40): 
-                        self.continue_button.get_clicked(self._create_output_window, self.board_size, self.number_of_figures, self.coords)
-                    if (self.row >= self.board_size or self.column >= self.board_size): continue
-                    self.attacked = self.board_maker.attacked_cell(self.row, self.column, self.board_size)
-
-                    if event.dict['button'] == 1:
-                        
-                        if not((self.row >= self.board_size or self.column >= self.board_size)):
-                            if self.board_grid[self.row][self.column] == '#' or self.board_grid[self.row][self.column] == '*' or self.number_of_figures == 0:
-                                continue
-                            
-                            for cell in self.attacked:
-                                self.board_grid[cell[0]][cell[1]] = '*'
-                                if self.all_attacked.get(cell) == None:
-                                    self.all_attacked[cell] = 1
-                                else: self.all_attacked[cell] +=1
-
-                            self.coords.append((self.row, self.column))
-                            self.board_grid[self.row][self.column] = '#'
-                            self.number_of_figures -= 1
-
-                    elif event.dict['button'] == 3:
-                        if self.board_grid[self.row][self.column] == '#':
-                            for cell in self.attacked:
-                                self.all_attacked[cell] -= 1
-                                if self.all_attacked[cell] == 0: 
-                                    self.board_grid[cell[0]][cell[1]] = 0
-                                    del self.all_attacked[cell]
-
-                        if self.board_grid[self.row][self.column] == '#':
-                            del self.coords[self.coords.index((self.row, self.column))]
-                            self.board_grid[self.row][self.column] = 0
-                            self.number_of_figures +=1
-
-            self.clock.tick(FPS)
-            pygame.display.update()
 
 if __name__ == '__main__':
     output = open('semester 2\Course work\output.txt', 'w+')
@@ -299,6 +320,4 @@ if __name__ == '__main__':
     app.mainloop()
     output.close()
     
-    
-    # N, L, K = file[0].split()
-    # standing_figure = [tuple([int(file[x].split()[0]), int(file[x].split()[1])]) for x in range(1, int(K) + 1)]
+
